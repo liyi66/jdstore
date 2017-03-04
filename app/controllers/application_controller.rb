be7037ae6834b109565cc2131afa38c51e75f335
pengxiaofeng <qhpbdt66@gmail.com>
2017-03-04 12:05:47 +0800

    error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,20 +3,19 @@ class ApplicationController < ActionController::Base
     def admin_required
         redirect_to '/', alert: 'You are not admin.' unless current_user.admin?
     end
-      helper_method :current_cart
 
-  def current_cart
-    @current_cart ||= find_cart
-  end
+    helper_method :current_cart
 
-  private
-
-  def find_cart
-    cart = Cart.find_by(id: session[:cart_id])
-    if cart.blank?
-      cart = Cart.create
+    def current_cart
+        @current_cart ||= find_cart
     end
-    session[:cart_id] = cart.id
-    return cart
-  end
+
+    private
+
+    def find_cart
+        cart = Cart.find_by(id: session[:cart_id])
+        cart = Cart.create if cart.blank?
+        session[:cart_id] = cart.id
+        cart
+    end
 end
